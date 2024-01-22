22ace76d5a8e2f0226833341451d393cf48b4958
marcosmolinaaraoz <marcosmolinaaraoz@outlook.com>
2024-01-22 14:57:23 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,41 +4,43 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
+        </p> */}
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="flex flex-col gap-8 justify-item-center">
-
           <div className="flex flex-row gap-8 items-center justifi-item-center">
-            <div className="">
-              <h1 className="mb-4">Respuestos Citroen Xsara 2.0 nafta 2011</h1>
+            
+            <div className="p-6 rounded-md shadow-md">
+              <h1 className="text-2xl font-bold mb-4 text-center">
+                Repuestos Citroen Xsara 2.0 nafta 2011
+              </h1>
               <ul>
-              <li>JUNTA (JGOX2) TAPA VALV.ESC ADM SABO</li>
-              <li>RETEN ARBOL DE LEVAS 405/PARTNER </li>
-              <li>RETEN ARBOL DE LEVAS ADM</li>
-              <li>KIT DISTRIBUVION SKF PEUGEOT 307/308</li>
-              <li>BOMBA DE AGUA SKF VKPA83650</li>
-              <li>AMORTIGUADOR TRAS.</li>
-              <li>BOTADOR HIDRAULICO ORIGINAL</li>
-            </ul>
-              </div>
+                <li className="text-sm">Junta (JGOX2) Tapa Valv. Esc Adm Sabo</li>
+                <li className="text-sm">Reten Arbol de Levas 405/Partner</li>
+                <li className="text-sm">Reten Arbol de Levas Adm</li>
+                <li className="text-sm">Kit Distribución SKF Peugeot 307/308</li>
+                <li className="text-sm">Bomba de Agua SKF VKPA83650</li>
+                <li className="text-sm">Amortiguador Trasero</li>
+                <li className="text-sm">Botador Hidráulico Original</li>
+              </ul>
+            </div>
             <Image
-              className="rounded rounded-lg"
+              className="rounded-lg"
               src="/auto.jpg"
               alt="Next.js Logo"
-              width={180}
-              height={37}
+              width={250}
+              height={50}
               priority
             />
           </div>
         </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -106,7 +108,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }

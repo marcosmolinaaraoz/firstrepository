e66d879a694670f1537770e62421b7f2d9b84ca3
marcosmolinaaraoz <marcosmolinaaraoz@outlook.com>
2024-01-22 13:33:39 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
          <Image
-          className="rounded rounded-lg animate-ping"
+          className="rounded rounded-lg"
           src="/moto.jpeg"
           alt="Next.js Logo"
           width={180}
@@ -40,7 +40,15 @@ export default function Home() {
         <div className='flex flex-col gap-8 justify-item-center'>
         <h1 className='text-6xl bg-neutral-800 animate-pulse'>Hello World</h1>
         <h3>Mi primera página web</h3>
-
+        <ul>
+          <li>JUNTA (JGOX2) TAPA VALV.ESC ADM SABO</li>
+          <li>RETEN ARBOL DE LEVAS 405/PARTNER </li>
+          <li>RETEN ARBOL DE LEVAS ADM</li>
+          <li>KIT DISTRIBUVION SKF PEUGEOT 307/308</li>
+          <li>BOMBA DE AGUA SKF VKPA83650</li>
+          <li>AMORTIGUADOR TRAS.</li>
+          <li>BOTADOR HIDRAULICO ORIGINAL</li>
+        </ul>
         </div>
       </div>
 

77bd50597c08f68aae306ec0f79b27dae781fde0
marcosmolinaaraoz <marcosmolinaaraoz@outlook.com>
2024-01-22 14:15:48 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,47 +8,33 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-           <Image
-              src="/motogp.jpeg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-         <Image
-          className="rounded rounded-lg"
-          src="/moto.jpeg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className='flex flex-col gap-8 justify-item-center'>
-        <h1 className='text-6xl bg-neutral-800 animate-pulse'>Hello World</h1>
-        <h3>Mi primera página web</h3>
-        <ul>
-          <li>JUNTA (JGOX2) TAPA VALV.ESC ADM SABO</li>
-          <li>RETEN ARBOL DE LEVAS 405/PARTNER </li>
-          <li>RETEN ARBOL DE LEVAS ADM</li>
-          <li>KIT DISTRIBUVION SKF PEUGEOT 307/308</li>
-          <li>BOMBA DE AGUA SKF VKPA83650</li>
-          <li>AMORTIGUADOR TRAS.</li>
-          <li>BOTADOR HIDRAULICO ORIGINAL</li>
-        </ul>
+        <div className="flex flex-col gap-8 justify-item-center">
+
+          <div className="flex flex-row gap-8 items-center justifi-item-center">
+            <div className="">
+              <h1 className="mb-4">Respuestos Citroen Xsara 2.0 nafta 2011</h1>
+              <ul>
+              <li>JUNTA (JGOX2) TAPA VALV.ESC ADM SABO</li>
+              <li>RETEN ARBOL DE LEVAS 405/PARTNER </li>
+              <li>RETEN ARBOL DE LEVAS ADM</li>
+              <li>KIT DISTRIBUVION SKF PEUGEOT 307/308</li>
+              <li>BOMBA DE AGUA SKF VKPA83650</li>
+              <li>AMORTIGUADOR TRAS.</li>
+              <li>BOTADOR HIDRAULICO ORIGINAL</li>
+            </ul>
+              </div>
+            <Image
+              className="rounded rounded-lg"
+              src="/auto.jpg"
+              alt="Next.js Logo"
+              width={180}
+              height={37}
+              priority
+            />
+          </div>
         </div>
       </div>
 
@@ -60,7 +46,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -77,7 +63,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -94,7 +80,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -111,7 +97,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -122,5 +108,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
